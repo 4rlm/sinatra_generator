13a20e1e30fe30958af22a80fcdf735ca97fc201
Adam Booth <4rlm@protonmail.ch>
2017-10-08 10:40:48 -0500

    fine tuned css.  all working perfectly.  next step explore joined tables with tag.

--- generator/file_writer.rb
+++ generator/file_writer.rb
@@ -4,9 +4,34 @@ require 'pry'
 module FileWriter
   extend ActiveSupport::Concern
 
+
   def snake_to_cap_string(snake_case)
     snake_case = snake_case.downcase.tr("_", " ") if snake_case.include?('_')
     capitalized_strings = snake_case.split.map(&:capitalize).join(' ')
+  end
+
+  def field_extractor(snake_case)
+    raw_fields = @mvc_hashes.find { |hash| hash[:table] == snake_case }[:fields]
+    fields = raw_fields.map { |field_pair| field_pair.split(':').first }
+  end
+
+  def extract_mvc_hashes(snake_case)
+    selected_table_data_hash = @mvc_hashes.find { |hash| hash[:table] == snake_case }
+    fields = selected_table_data_hash[:fields]
+    table_fields = []
+
+    fields.each do |field|
+      if field.include?(':')
+        field_key_and_type = field.split(':')
+        f_key = field_key_and_type[0]
+        f_type = field_key_and_type[1]
+      else
+        f_key = field
+        f_type = 'string'
+      end
+      table_fields << "t.#{f_type} :#{f_key}\n\t\t\t"
+    end
+    string_table_fields = table_fields.join("")
   end
 
   def generate_model_file(snake_case, model_name)
@@ -22,28 +47,6 @@ module FileWriter
     end
   end
 
-  def extract_mvc_hashes(snake_case)
-    selected_table_data_hash = @mvc_hashes.find { |hash| hash[:table] == snake_case }
-
-    fields = selected_table_data_hash[:fields]
-    table_fields = []
-
-    fields.each do |field|
-      if field.include?(':')
-        field_key_and_type = field.split(':')
-        f_key = field_key_and_type[0]
-        f_type = field_key_and_type[1]
-      else
-        f_key = field
-        f_type = 'string'
-      end
-      table_fields << "t.#{f_type} :#{f_key}\n\t\t\t"
-    end
-
-    string_table_fields = table_fields.join("")
-  end
-
-
   def generate_migration_file(snake_case, migration_name)
     snake_plural = snake_case.pluralize
     camel_plural = migration_name.pluralize
@@ -52,7 +55,6 @@ module FileWriter
     puts "Creating #{migration_path}"
 
     string_table_fields = extract_mvc_hashes(snake_case)
-
     File.open(migration_path, 'w+') do |f|
       f.write(<<-EOF.gsub(/^ {8}/, ''))
         class Create#{camel_plural} < ActiveRecord::Migration[5.0]
@@ -117,7 +119,6 @@ module FileWriter
 
       EOF
     end
-
   end
 
   def generate_application_controller
@@ -136,7 +137,6 @@ module FileWriter
     puts "Creating #{application_controller_path}"
 
     route_strings = routes.join("")
-
     File.open(application_controller_path, 'w+') do |f|
       f.write(<<-EOF.gsub(/^ {6}/, ''))
       class ApplicationController < Sinatra::Base
@@ -166,13 +166,10 @@ module FileWriter
 
       EOF
     end
-
   end
-
 
   def generate_controller_file(snake_case, camelize)
     snake_plural = snake_case.pluralize
-
     controller_path = "../#{@app_name}/app/controllers/#{snake_plural}_controller.rb"
     puts "Creating #{controller_path}"
 
@@ -243,10 +240,8 @@ module FileWriter
     end
   end
 
-
   def generate_config_ru  ## config.ru (in root)
     config_ru_path = "../#{@app_name}/config.ru"
-
     formatted_controllers = []
     @mvc.each do |snake_case|
       formatted_controllers << "use #{snake_case.pluralize.camelize}Controller\n"
@@ -254,7 +249,6 @@ module FileWriter
 
     puts "Creating #{config_ru_path}"
     controllers_list = formatted_controllers.join("")
-
     File.open(config_ru_path, 'w+') do |f|
       f.write(<<-EOF.gsub(/^ {6}/, ''))
       require_relative './config/environment'
@@ -272,18 +266,24 @@ module FileWriter
   end
 
 
+
+
+  ###### GENERATE VIEWS FILES BELOW ######
+
   def generate_view_files(snake_case, camelized)
     snake_plural = snake_case.pluralize
-    fields = @mvc_hashes.find { |hash| hash[:table] == snake_case }[:fields]
+    # fields = @mvc_hashes.find { |hash| hash[:table] == snake_case }[:fields]
+    fields = field_extractor(snake_case)
 
     FileUtils.mkdir_p("../#{@app_name}/app/views/#{snake_plural}")
     generate_index_view(snake_case, camelized, snake_plural, fields)
+    generate_show_view(snake_case, camelized, snake_plural, fields)
+    generate_form_fields(snake_case, camelized, snake_plural, fields)
     generate_new_view(snake_case, camelized, snake_plural)
     generate_edit_view(snake_case, camelized, snake_plural)
-    generate_show_view(snake_case, camelized, snake_plural, fields)
     generate_delete_view(snake_case, camelized, snake_plural)
-    generate_form_fields(snake_case, camelized, snake_plural, fields)
   end
+
 
   def generate_form_fields(snake_case, camelized, snake_plural, fields)
     form_path = "../#{@app_name}/app/views/#{snake_plural}/_form_fields.erb"
@@ -311,10 +311,7 @@ module FileWriter
 
       EOF
     end
-
   end
-
-
 
   def generate_new_view(snake_case, camelized, snake_plural)
     view_path = "../#{@app_name}/app/views/#{snake_plural}/new.erb"
@@ -325,7 +322,7 @@ module FileWriter
       <div class="">
 
         <div class="erb_header">
-          <h2>Create New #{snake_to_cap_string(snake_case)}</h2>
+          <h3>Create New #{snake_to_cap_string(snake_case)}</h3>
         </div>
 
         <form class="form-horizontal" action="/#{snake_plural}" method="post">
@@ -340,7 +337,6 @@ module FileWriter
 
       EOF
     end
-
   end
 
   def generate_edit_view(snake_case, camelized, snake_plural)
@@ -352,7 +348,7 @@ module FileWriter
       <div class="">
 
         <div class="erb_header">
-          <h2>Edit #{snake_to_cap_string(snake_case)}</h2>
+          <h3>Edit #{snake_to_cap_string(snake_case)}</h3>
         </div>
 
         <form class="form-horizontal" action="/#{snake_plural}/<%= @#{snake_case}.id %>" method="post">
@@ -384,7 +380,6 @@ module FileWriter
       EOF
     end
 
-
     td_blocks = []
     fields.each do |field|
       td_blocks << (<<-EOF.gsub(/^ {6}/, ''))
@@ -407,7 +402,7 @@ module FileWriter
           <tr>
             <th class="w-small">ID</th>
       #{th_block_strings}
-            <th class="w-small">Posted/Updated</th>
+            <th class="w-med">Updated</th>
           </tr>
 
           <tr>
@@ -420,9 +415,7 @@ module FileWriter
       </div>
       EOF
     end
-
   end
-
 
   def generate_index_view(snake_case, camelized, snake_plural, fields)
     view_path = "../#{@app_name}/app/views/#{snake_plural}/index.erb"
@@ -449,16 +442,16 @@ module FileWriter
       f.write(<<-EOF.gsub(/^ {6}/, ''))
       <h3>View All #{snake_to_cap_string(snake_case)}</h3>
 
-      <table class="table table-bordered table-striped table-hover">
+      <table class="table table-bordered table-hover table-striped">
         <tr>
-          <th>ID</th>
+          <th class="w-small">ID</th>
       #{th_block_strings}
-          <th>Updated</th>
-          <th colspan="3">Manage</th>
+          <th class="w-med">Updated</th>
+          <th colspan="3" class="w-med">Manage</th>
         </tr>
         <% @#{snake_plural}.each do |#{snake_case}|%>
         <tr>
-          <td><%= #{snake_case}.id%></td>
+          <td class="w-small"><%= #{snake_case}.id%></td>
       #{td_block_strings}
           <td><%= #{snake_case}.updated_at.strftime('%x')%></td>
 
@@ -479,7 +472,6 @@ module FileWriter
 
       EOF
     end
-
   end
 
   def generate_delete_view(snake_case, camelized, snake_plural)
@@ -494,20 +486,10 @@ module FileWriter
       </form>
       EOF
     end
-
   end
-
-
-  ################
 
   def generate_application_layout_view
     li_links = []
-    # @mvc.each do |snake_case|
-    #   snake_plural = snake_case.pluralize
-    #   li_links << "<li><a href='/#{snake_plural}'>#{snake_plural}</a></li>\n\t\t\t\t\t\t\t"
-    #   li_links << "<li><a href='/#{snake_plural}'>#{snake_plural}</a></li>\n\t\t\t\t\t\t\t"
-    # end
-
     @mvc.each do |snake_case|
       snake_plural = snake_case.pluralize
       camelized = snake_case.camelize
@@ -635,6 +617,8 @@ module FileWriter
     puts "Creating #{seed_path}"
     # tables = @mvc_hashes.map { |hash| hash[:table] }
 
+    # fields = field_extractor(snake_case) # might need to use this.
+
     seed_model_blocks = []
     @mvc_hashes.each do |table_hash|
       model_name = table_hash[:table]
@@ -674,9 +658,8 @@ module FileWriter
       #{seed_model_block_strings}
       EOF
     end
-
-
   end
+
 
 
 
